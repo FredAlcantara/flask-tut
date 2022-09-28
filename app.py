62617450f281fa87572a6374506afcf81f035401
FredAlcantara <fred.alcantara@city.ac.uk>
2022-09-28 21:06:38 +0100

    created login function for GET and POST

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 from flask import Flask, redirect, url_for, render_template
 
-app = Flask (__name__)
+app = Flask(__name__)
+
 
 @app.route("/index")
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    return render_template()
+
+
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1>{usr}</h1>"
 
 
 if __name__ == "__main__":
